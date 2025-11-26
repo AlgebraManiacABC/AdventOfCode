e69a8df6489be5df1afd82f520a2b8233d2e4829
AlgebraManiacABC <dunnluke01@gmail.com>
2025-11-25 18:39:27 -0600

    Update fetch.py to create template c file; new utility aoc.c/.h

--- fetch.py
+++ fetch.py
@@ -7,6 +7,15 @@ from aocd import get_data
 os.environ['SSL_CERT_FILE'] = certifi.where()
 os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
 
+C_TEMPLATE ='''\
+#include <stdio.h>
+#include "../../aoc.h"
+
+int main(int argc, char * argv[])
+{
+}
+'''
+
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print(f"Usage:\n\t{sys.argv[0]} <d> <yyyy>")
@@ -21,7 +30,10 @@ if __name__ == '__main__':
         Path.mkdir(Path(f"./{year}"),mode=0o775)
     day_folder = Path(f"./{year}/{day}")
     if not Path.exists(day_folder):
-        Path.mkdir(Path(f"{Path.cwd()}/{year}/{day}"),mode=0o775)
+        Path.mkdir(Path(f"./{year}/{day}"),mode=0o775)
 
-    Path.write_text(Path(f"{Path.cwd()}/{year}/{day}/input.txt"),
+    Path.write_text(Path(f"./{year}/{day}/input.txt"),
                      get_data(day=day,year=year))
+
+    # Also prepare day.c
+    Path.write_text(Path(f"./{year}/{day}/{day}.c"),C_TEMPLATE)
